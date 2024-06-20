b279f77550d7f0451e4bfaab4c24ae0bdc6f5013
yeongjuyun <ykkc777@deepnoid.com>
2024-06-20 09:32:11 +0900

    refactor: useSocket 리팩토링

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -28,63 +28,12 @@ const useSocket = ({
   const socketRef = useRef<Socket | null>(null);
   const userVideo = useRef<HTMLVideoElement | null>(null);
   const peersRef = useRef<{ peerID: string; peer: Instance }[]>([]);
+  const streamRef = useRef<MediaStream | undefined>(undefined);
 
   useEffect(() => {
     socketRef.current = io("http://localhost:8080");
     socketRef.current.on("connect", () => {
-      navigator.mediaDevices
-        .getUserMedia({ video: true, audio: true })
-        .then((stream) => {
-          if (userVideo.current) {
-            userVideo.current.srcObject = stream;
-          }
-
-          if (socketRef.current) {
-            socketRef.current.emit("join", { roomId, username });
-            socketRef.current.on("all users", (users: User[]) => {
-              const peers: any[] = [];
-              users.forEach((user) => {
-                const peer = createPeer(user.id, socketRef.current?.id, stream);
-                peersRef.current.push({
-                  peerID: user.id,
-                  peer,
-                });
-                peers.push(peer);
-              });
-              setPeers(peers);
-            });
-
-            socketRef.current.on("user joined", (payload) => {
-              const peer = addPeer(payload.signal, payload.callerID, stream);
-              peersRef.current.push({
-                peerID: payload.callerID,
-                peer,
-              });
-
-              setPeers((peers) => [...peers, peer]);
-            });
-
-            socketRef.current.on("receiving returned signal", (payload) => {
-              const item = peersRef.current.find(
-                (p) => p.peerID === payload.id
-              );
-              item?.peer.signal(payload.signal);
-            });
-
-            socketRef.current.on("disconnect user", ({ userId }) => {
-              const remainingPeers = peersRef.current.filter((p) => {
-                if (p.peerID === userId) {
-                  p.peer.destroy();
-                  return false;
-                }
-                return true;
-              });
-
-              peersRef.current = remainingPeers;
-              setPeers(remainingPeers.map((p) => p.peer));
-            });
-          }
-        });
+      initMediaConnection();
     });
 
     socketRef.current.on("chat", (msg: Message) => {
@@ -102,11 +51,34 @@ const useSocket = ({
     };
   }, [username, roomId]);
 
-  const createPeer = (userToSignal: any, callerID: any, stream: any) => {
+  const initMediaConnection = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        if (userVideo.current) {
+          userVideo.current.srcObject = stream;
+        }
+        streamRef.current = stream;
+
+        if (socketRef.current) {
+          socketRef.current?.emit("join", { roomId, username });
+          socketRef.current.on("all users", getUsers);
+          socketRef.current.on("user joined", joinUser);
+          socketRef.current.on("receiving returned signal", getSignal);
+          socketRef.current.on("disconnect user", deleteUser);
+        }
+      });
+  };
+
+  const createPeer = (
+    userToSignal: string,
+    callerID: string,
+    stream: MediaStream
+  ) => {
     const peer = new Peer({
       initiator: true,
       trickle: false,
-      stream,
+      stream: stream,
     });
 
     peer.on("signal", (signal) => {
@@ -124,17 +96,21 @@ const useSocket = ({
     });
 
     peer.on("close", () => {
-      console.log("Create - Peer connection closed");
+      console.log("Peer connection closed");
     });
 
     return peer;
   };
 
-  const addPeer = (incomingSignal: string, callerID: string, stream: any) => {
+  const addPeer = (
+    incomingSignal: string,
+    callerID: string,
+    stream: MediaStream
+  ) => {
     const peer = new Peer({
       initiator: false,
       trickle: false,
-      stream,
+      stream: stream,
     });
 
     peer.on("signal", (signal) => {
@@ -151,10 +127,55 @@ const useSocket = ({
     });
 
     peer.on("close", () => {
-      console.log("Add - Peer connection closed");
+      console.log("Peer connection closed");
     });
 
     return peer;
+  };
+
+  const joinUser = (payload: { callerID: string; signal: any }) => {
+    const peer = addPeer(payload.signal, payload.callerID, streamRef.current!);
+    peersRef.current.push({
+      peerID: payload.callerID,
+      peer,
+    });
+
+    setPeers((peers) => [...peers, peer]);
+  };
+
+  const getUsers = (users: User[]) => {
+    const peers: any[] = [];
+    users.forEach((user) => {
+      const peer = createPeer(
+        user.id,
+        socketRef.current?.id!,
+        streamRef.current!
+      );
+      peersRef.current.push({
+        peerID: user.id,
+        peer,
+      });
+      peers.push(peer);
+    });
+    setPeers(peers);
+  };
+
+  const getSignal = (payload: { id: string; signal: any }) => {
+    const item = peersRef.current.find((p) => p.peerID === payload.id);
+    item?.peer.signal(payload.signal);
+  };
+
+  const deleteUser = (payload: { userId: string }) => {
+    const remainingPeers = peersRef.current.filter((p) => {
+      if (p.peerID === payload.userId) {
+        p.peer.destroy();
+        return false;
+      }
+      return true;
+    });
+
+    peersRef.current = remainingPeers;
+    setPeers(remainingPeers.map((p) => p.peer));
   };
 
   const sendMessage = (username: string, message: string) => {
@@ -172,13 +193,13 @@ const useSocket = ({
   };
 
   return {
-    messages,
     users,
+    peers,
     micOn,
+    messages,
+    userVideo,
     sendMessage,
     toggleMic,
-    userVideo,
-    peers,
   };
 };
 
